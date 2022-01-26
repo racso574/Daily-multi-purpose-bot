1c840503f9b75300cbd06c1aa3704ec5848bbf53
racso574 <96116063+racso574@users.noreply.github.com>
2022-01-26 20:14:52 +0100

    Add files via upload

--- cogs/misiones.py
+++ cogs/misiones.py
@@ -18,19 +18,59 @@ class misiones(commands.Cog):
         xl = load_workbook('./cogs/db/misiones.xlsx')
         edit = xl['Sheet1']
 
-        t1 = str(edit['a1'].value)
-        t2 = str(edit['a2'].value)
-        t3 = str(edit['a3'].value)
-        t4 = str(edit['a4'].value)
-        t5 = str(edit['a4'].value)
+        n1 = int('1')
+        n2 = int('2')
+        n3 = int('3')
+        n4 = int('4')
+        n5 = int('5')
+
+        i1 = edit.cell(row=n1, column=1).value
+        i2 = edit.cell(row=n2, column=1).value
+        i3 = edit.cell(row=n3, column=1).value
+        i4 = edit.cell(row=n4, column=1).value
+        i5 = edit.cell(row=n5, column=1).value
 
         embed = discord.Embed(title="MISIONES", description=f"")
-        embed.add_field(name='1', value=t1, inline=False)
-        embed.add_field(name='2', value=t2, inline=False)
-        embed.add_field(name='3', value=t3, inline=False)
-        embed.add_field(name='4', value=t4, inline=False)
-        embed.add_field(name='5', value=t5, inline=False)
-        await ctx.send(embed=embed)
+        embed.add_field(name=n1, value=i1, inline=False)
+        embed.add_field(name=n2, value=i2, inline=False)
+        embed.add_field(name=n3, value=i3, inline=False)
+        embed.add_field(name=n4, value=i4, inline=False)
+        embed.add_field(name=n5, value=i5, inline=False)
+        ms = await ctx.send(embed=embed)
+        await ms.add_reaction('◀️')
+        await ms.add_reaction('▶️')
+
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        channel = await self.bot.fetch_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        user = await self.bot.fetch_user(payload.user_id)
+        emoji = payload.emoji
+        xl = load_workbook('./cogs/db/misiones.xlsx')
+        edit = xl['Sheet1']
+        if channel.id == 935238981887619123 and user.id != 864541492281868320:
+            n1 = int('6')
+            n2 = int('7')
+            n3 = int('8')
+            n4 = int('9')
+            n5 = int('10')
+
+            i1 = str([edit.cell(row=n1, column=1).value])
+            i2 = str([edit.cell(row=n2, column=1).value])
+            i3 = str([edit.cell(row=n3, column=1).value])
+            i4 = str([edit.cell(row=n4, column=1).value])
+            i5 = str([edit.cell(row=n5, column=1).value])
+
+            embed = discord.Embed(title="MISIONES", description=f"")
+            embed.add_field(name=n1, value=i1, inline=False)
+            embed.add_field(name=n2, value=i2, inline=False)
+            embed.add_field(name=n3, value=i3, inline=False)
+            embed.add_field(name=n4, value=i4, inline=False)
+            embed.add_field(name=n5, value=i5, inline=False)
+
+            await message.edit(content=embed)
+
 
 
     @commands.command()
