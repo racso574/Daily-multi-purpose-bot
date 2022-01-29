38ca57bbaff36ffa8c1176413cce6ca2e6808500
racso574 <96116063+racso574@users.noreply.github.com>
2022-01-29 21:36:36 +0100

    Add files via upload

---
+++ cogs/diarias.py
@@ -0,0 +1,96 @@
+import discord
+from discord.ext import commands
+from googletrans import Translator
+from openpyxl import load_workbook
+import time
+from random import randint, choice
+
+
+
+
+class diarias(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def da(self, ctx):
+        xl = load_workbook('./cogs/db/misiones.xlsx')
+        edit = xl['Sheet1']
+
+        n1 = int('1')
+        n2 = int('2')
+        n3 = int('3')
+        n4 = int('4')
+        n5 = int('5')
+
+        i1 = edit.cell(row=n1, column=1).value
+        i2 = edit.cell(row=n2, column=1).value
+        i3 = edit.cell(row=n3, column=1).value
+        i4 = edit.cell(row=n4, column=1).value
+        i5 = edit.cell(row=n5, column=1).value
+
+        embed = discord.Embed(title="MISIONES", description=f"")
+        embed.add_field(name=n1, value=i1, inline=False)
+        embed.add_field(name=n2, value=i2, inline=False)
+        embed.add_field(name=n3, value=i3, inline=False)
+        embed.add_field(name=n4, value=i4, inline=False)
+        embed.add_field(name=n5, value=i5, inline=False)
+        ms = await ctx.send(embed=embed)
+        await ms.add_reaction('◀')
+        await ms.add_reaction('▶')
+        await ms.add_reaction('▶')
+        await ms.add_reaction('▶')
+
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        channel = await self.bot.fetch_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        user = await self.bot.fetch_user(payload.user_id)
+        emoji = payload.emoji
+        xl = load_workbook('./cogs/db/misiones.xlsx')
+        edit = xl['Sheet1']
+        if channel.id == 935238981887619123 and user.id != 864541492281868320:
+            print(emoji)
+
+
+
+
+
+    @commands.command()
+    async def nms(self, ctx):
+        xl = load_workbook('./cogs/db/misiones.xlsx')
+        edit = xl['Sheet1']
+
+        t1 = str(edit['a1'].value)
+        t2 = str(edit['a2'].value)
+        t3 = str(edit['a3'].value)
+        t4 = str(edit['a4'].value)
+        t5 = str(edit['a4'].value)
+
+        if t1 == 'None':
+            edit['a1'].value = ctx.message.content[4:]
+            xl.save('./cogs/db/misiones.xlsx')
+        elif t2 == 'None':
+            edit['a2'].value = ctx.message.content[4:]
+            xl.save('./cogs/db/misiones.xlsx')
+        elif t3 == 'None':
+            edit['a3'].value = ctx.message.content[4:]
+            xl.save('./cogs/db/misiones.xlsx')
+        elif t4 == 'None':
+            edit['a4'].value = ctx.message.content[4:]
+            xl.save('./cogs/db/misiones.xlsx')
+        elif t5 == 'None':
+            edit['a5'].value = ctx.message.content[4:]
+            xl.save('./cogs/db/misiones.xlsx')
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(diarias(bot))
